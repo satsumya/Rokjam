ed9b3cbebf9a700e4b502d6b616c2e5e3dfd43b0
Satsumya <iamsarahbarnes@gmail.com>
2026-07-14 07:24:55 +1000

    story book font align

--- .rnstorybook/preview.tsx
+++ .rnstorybook/preview.tsx
@@ -1,6 +1,18 @@
 import type { Preview } from '@storybook/react-native';
+import { useFonts } from 'expo-font';
+
+import { fontMap } from '../src/theme/fonts';
 
 const preview: Preview = {
+  decorators: [
+    (Story) => {
+      const [fontsLoaded, fontError] = useFonts(fontMap);
+      if (!fontsLoaded && !fontError) {
+        return null;
+      }
+      return <Story />;
+    },
+  ],
   parameters: {
     controls: {
       matchers: {
